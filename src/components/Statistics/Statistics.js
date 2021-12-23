434208a60f21d174af0384e3599abe02cd501bc2
OleksandrPivets <sashapivets@gmail.com>
2021-12-23 21:39:18 +0200

    fin

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+
+function Statistics({stats}) {
+    return(
+    <section className={s.statistics}>
+  <h2 className={s.title}>Upload stats</h2>
+  <ul className={s['stat-list']}>
+      {stats.map(item => (
+          <li key = {item.id} className={s.item}>
+          <span className={s.label}>{item.label}</span>
+          <span className={s.percentage}>{item.percentage}%</span>
+        </li>
+      ))}
+  </ul> 
+</section>
+)};
+
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  )};
+
+export default Statistics;
+
