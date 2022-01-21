422acd9e90e40d688b6c8abef7d87e8d56f7e6ad
OleksandrPivets <sashapivets@gmail.com>
2022-01-21 10:28:27 +0200

    fin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Profile from './components/Profile/Profile';
-import user from './components/Profile/user.json';
 import Statistics from './components/Statistics/Statistics';
-import data from './components/Statistics/data.json';
-import friends from './components/FriendList/friends.json';
 import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import transactions from './components/TransactionHistory/transactions.json';
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 function App() {
   return (
